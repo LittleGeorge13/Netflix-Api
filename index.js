88ad0dc85e14cba82dfa89d66b25aee041268b20
Jorge Esqueda Rocha <jorge_squiky@hotmail.com>
2026-07-28 09:43:33 -0600

    Create a connection to mongoDB with mongoose

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-app.listen(8800, () => {
-    console.log("Backend server is running!");
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.bhrgwvi.mongodb.net/${process.env.MONGO_DB_DATABASE}?appName=Cluster0`;
+
+mongoose.connect(MONGODB_URI).then(async () => {
+    app.listen(process.env.PORT || 8800, () => { console.log("Backend server is running!"); });
+}).catch(error => {
+    throw new Error(error);
 });
