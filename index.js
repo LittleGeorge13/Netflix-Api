5e4ddc23f55776a392bd7adb2d2bdba7d6ae4f65
Jorge Esqueda Rocha <jorge_squiky@hotmail.com>
2026-07-29 14:24:13 -0600

    Fixed CORS errors and adjusted endpoints urls

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ const userRoute = require('./routes/users');
 const movieRoute = require('./routes/movies');
 const listRoute = require('./routes/lists');
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+});
 app.use(express.json());
 app.use('/api/auth', authRoute);
-app.use('/users', userRoute);
-app.use('/movies', movieRoute);
-app.use('/lists', listRoute);
+app.use('/api/users', userRoute);
+app.use('/api/movies', movieRoute);
+app.use('/api/lists', listRoute);
 
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.bhrgwvi.mongodb.net/${process.env.MONGO_DB_DATABASE}?appName=Cluster0`;
 
